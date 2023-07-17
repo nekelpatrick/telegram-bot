7a1f1ffa4e62499356e5da51fe7316d67b3c1937
Patrick Nekel <nekelpatrick@gmail.com>
2023-07-17 16:47:53 -0300

    clean code and add tweaks

--- src/commands/baixarCommand.ts
+++ src/commands/baixarCommand.ts
@@ -2,22 +2,39 @@
 import { MyContext } from "..";
 import { wavDownloader } from "../wav-downloader";
 
-export async function baixarCommand(ctx: MyContext) {
+export async function baixarCommand(ctx: any) {
   console.log('Processing "baixar" command');
+
+  await ctx.reply("Processando comando 'baixar'...");
 
   await ctx.reply("Validando URLs...");
   console.log("Validating URLs");
 
-  await wavDownloader(ctx.message?.text, ctx);
-  console.log("URLs processed");
+  try {
+    await wavDownloader(ctx.message?.text, ctx);
+  } catch (error) {
+    console.log(`Erro ao baixar músicas: ${error}`);
+    await ctx.reply(`Ocorreu um erro ao baixar as músicas`);
+    return;
+  }
 
-  await Promise.all([
-    ctx.reply("Músicas processadas."),
-    ctx.reply("👍"),
-    ctx.reply(
-      "Baixar mais músicas? \n- /baixar: Baixar músicas a partir de URLs do YouTube"
-    ),
-  ]);
+  console.log("URLs processed");
+  await ctx.reply("URLs processadas.");
+
+  await ctx.reply("Baixando músicas...");
+
+  // You can add here additional replies based on the wavDownloader function
+  // results or progress, for example, when a song download starts or finishes.
+
+  await ctx.reply("Músicas baixadas. Processando...");
+
+  // You could add more steps here to inform the user about the processing status
+
+  await ctx.reply("Músicas processadas. Pronto para ouvir! 👍");
+
+  await ctx.reply(
+    "Deseja baixar mais músicas? \n- /baixar: Baixar músicas a partir de URLs do YouTube"
+  );
 
   // Reset the session command
   ctx.session.command = undefined;
--- src/index.ts
+++ src/index.ts
@@ -8,83 +8,62 @@ dotenv.config();
 interface SessionData {
   started?: boolean;
   command?: string;
-  pizzaCount?: number;
 }
 
 export type MyContext = SessionFlavor<SessionData> & Context;
 
 const bot = new Bot<MyContext>(process.env.TELEGRAM_TOKEN || "");
 
-bot.start();
+const initialSessionData: any = { started: false };
 
-function initial(): SessionData {
-  return { started: false };
-}
+bot.use(session({ initial: initialSessionData }));
 
-bot.use(session({ initial }));
-
-bot.command("start", (ctx) => {
+const greetingCommand = (ctx: MyContext) => {
   ctx.session.started = true;
-  return ctx.reply("Started");
-});
+  return ctx.reply(`Olá ${ctx.from?.username}`);
+};
 
-bot.command("ola", (ctx) => {
-  ctx.session.started = true;
-  ctx.reply(`Olá ${ctx.from?.username}`);
-});
+bot.command("start", greetingCommand);
+bot.command("ola", greetingCommand);
 
 bot.command("baixar", (ctx) => {
   ctx.session.started = true;
-
-  console.log('Received command "baixar"');
-  ctx.reply("Mande um ou vários links do YouTube.");
   ctx.session.command = "baixar";
+  return ctx.reply("Mande um ou vários links do YouTube.");
 });
 
 bot.on("message", async (ctx) => {
-  console.log("Received a message");
-  console.log(ctx.session);
-
-  if (ctx.session.command === "baixar") {
+  if (ctx.session.command === "baixar" && ctx.message.text) {
     await baixarCommand(ctx);
+  } else {
+    replyWithIntro(ctx);
   }
+});
+
+const introductionMessage = `Aqui estão todos os comandos disponíveis:
+- /baixar: Baixar músicas a partir de URLs do YouTube`;
+
+const replyWithIntro = (ctx: MyContext) => ctx.reply(introductionMessage);
+bot.command("start", replyWithIntro);
+
+bot.catch((err) => {
+  console.error(`Ocorreu um erro: ${err}`);
 });
 
 bot.api.setMyCommands([
   { command: "ola", description: "Be greeted by the bot" },
-  {
-    command: "baixar",
-    description: "Baixar uma ou várias músicas",
-  },
+  { command: "baixar", description: "Baixar uma ou várias músicas" },
 ]);
 
-const replyWithIntro = (ctx: MyContext) => ctx.reply(introductionMessage);
+if (process.env.NODE_ENV === "production") {
+  const app = express();
+  app.use(express.json());
+  app.use(webhookCallback(bot, "express"));
 
-bot.command("start", replyWithIntro);
-bot.catch(async (err) => {
-  console.error(`Ocorreu um erro: ${err}`);
-});
-
-// dotenv.config();
-
-console.log(process.env.GOOGLE_DRIVE_CLIENT_ID);
-console.log(process.env.GOOGLE_DRIVE_CLIENT_SECRET);
-console.log(process.env.GOOGLE_DRIVE_REDIRECT_URI);
-console.log(process.env.GOOGLE_DRIVE_REFRESH_TOKEN);
-
-// if (process.env.NODE_ENV === "production") {
-//   // Use Webhooks for the production server
-//   const app = express();
-//   app.use(express.json());
-//   app.use(webhookCallback(bot, "express"));
-
-//   const PORT = process.env.PORT || 3000;
-//   app.listen(PORT, () => {
-//     console.log(`Bot listening on port ${PORT}`);
-//   });
-// } else {
-//   // Use Long Polling for development
-// }
-
-const introductionMessage = `Aqui estão todos os comandos disponíveis:
-- /baixar: Baixar músicas a partir de URLs do YouTube`;
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Bot listening on port ${PORT}`);
+  });
+} else {
+  bot.start();
+}
