a226f703102d7f720a176c1e3be39f1300a579bf
Patrick Nekel <nekelpatrick@gmail.com>
2023-07-17 17:22:35 -0300

    rollback changes to index.ts

--- src/index.ts
+++ src/index.ts
@@ -8,54 +8,72 @@ dotenv.config();
 interface SessionData {
   started?: boolean;
   command?: string;
+  pizzaCount?: number;
 }
 
 export type MyContext = SessionFlavor<SessionData> & Context;
 
 const bot = new Bot<MyContext>(process.env.TELEGRAM_TOKEN || "");
 
-const initialSessionData: any = { started: false };
+function initial(): SessionData {
+  return { started: false };
+}
 
-bot.use(session({ initial: initialSessionData }));
+bot.use(session({ initial }));
 
-const greetingCommand = (ctx: MyContext) => {
+bot.command("start", (ctx) => {
   ctx.session.started = true;
-  return ctx.reply(`Olá ${ctx.from?.username}`);
-};
+  return ctx.reply("Started");
+});
 
-bot.command("start", greetingCommand);
-bot.command("ola", greetingCommand);
+bot.command("ola", (ctx) => {
+  ctx.session.started = true;
+  ctx.reply(`Olá ${ctx.from?.username}`);
+});
 
 bot.command("baixar", (ctx) => {
   ctx.session.started = true;
+
+  console.log('Received command "baixar"');
+  ctx.reply("Mande um ou vários links do YouTube.");
   ctx.session.command = "baixar";
-  return ctx.reply("Mande um ou vários links do YouTube.");
 });
 
 bot.on("message", async (ctx) => {
-  if (ctx.session.command === "baixar") {
-    await baixarCommand(ctx);
-  } else {
+  console.log("Received a message");
+  console.log(ctx.session);
+
+  if (ctx.session.command === "baixar" && ctx.message.text) {
+    await baixarCommand(ctx.message.text);
+  } else if (ctx.session.command !== "baixar") {
     replyWithIntro(ctx);
   }
 });
 
-const introductionMessage = `Aqui estão todos os comandos disponíveis:
-- /baixar: Baixar músicas a partir de URLs do YouTube`;
+bot.api.setMyCommands([
+  { command: "ola", description: "Be greeted by the bot" },
+  {
+    command: "baixar",
+    description: "Baixar uma ou várias músicas",
+  },
+]);
 
 const replyWithIntro = (ctx: MyContext) => ctx.reply(introductionMessage);
-bot.command("start", replyWithIntro);
 
-bot.catch((err) => {
+bot.command("start", replyWithIntro);
+bot.catch(async (err) => {
   console.error(`Ocorreu um erro: ${err}`);
 });
 
-bot.api.setMyCommands([
-  { command: "ola", description: "Be greeted by the bot" },
-  { command: "baixar", description: "Baixar uma ou várias músicas" },
-]);
+// dotenv.config();
+
+console.log(process.env.GOOGLE_DRIVE_CLIENT_ID);
+console.log(process.env.GOOGLE_DRIVE_CLIENT_SECRET);
+console.log(process.env.GOOGLE_DRIVE_REDIRECT_URI);
+console.log(process.env.GOOGLE_DRIVE_REFRESH_TOKEN);
 
 if (process.env.NODE_ENV === "production") {
+  // Use Webhooks for the production server
   const app = express();
   app.use(express.json());
   app.use(webhookCallback(bot, "express"));
@@ -65,5 +83,9 @@ if (process.env.NODE_ENV === "production") {
     console.log(`Bot listening on port ${PORT}`);
   });
 } else {
+  // Use Long Polling for development
   bot.start();
 }
+
+const introductionMessage = `Aqui estão todos os comandos disponíveis:
+- /baixar: Baixar músicas a partir de URLs do YouTube`;
