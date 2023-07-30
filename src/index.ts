ce443e853b756733caf8e78a1b5295fc73edfecd
Patrick Nekel <nekelpatrick@gmail.com>
2023-07-29 21:58:27 -0300

    update api domain

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,17 @@
 import express from "express";
 import { Bot, Context, SessionFlavor, session, webhookCallback } from "grammy";
 import dotenv from "dotenv";
-import { baixarCommand } from "./commands/baixarCommand";
 import axios from "axios";
-import qs from "qs";
 
 interface SessionData {
   started?: boolean;
   command?: string;
-  pizzaCount?: number;
 }
 
 export type MyContext = SessionFlavor<SessionData> & Context;
 
 // BOT INITIALIZATION
+dotenv.config();
 const bot = new Bot<MyContext>(process.env.TELEGRAM_TOKEN || "");
 
 // SESSION INITIALIZATION
@@ -25,40 +23,70 @@ bot.use(session({ initial }));
 
 // BOT COMMANDS
 const botCommands = [
-  { command: "ola", description: "Be greeted by the bot" },
-  { command: "baixar", description: "Baixar uma ou várias músicas" },
+  // { command: "ola", description: "Be greeted by the bot" },
+  { command: "baixar", description: "Baixar musicas" },
 ];
 
 bot.api.setMyCommands(botCommands);
 
 // COMMAND HANDLERS
 bot.command("start", (ctx) => ctx.reply(introductionMessage));
-bot.command("ola", (ctx) => ctx.reply(`Olá ${ctx.from?.username}`));
+bot.command("ola", (ctx) => ctx.reply(`Hello ${ctx.from?.username}`));
 bot.command("baixar", handleBaixarCommand);
-bot.on("message", handleMessages);
-bot.catch(async (err) => console.error(`Ocorreu um erro: ${err}`));
+
+bot.catch(async (err) => console.error(`An error occurred: ${err}`));
 
 // HANDLERS FUNCTIONS
 function handleBaixarCommand(ctx: MyContext) {
   console.log('Received command "baixar"');
-  ctx.reply("Mande um ou vários links do YouTube.");
+  ctx.reply("Mande um ou mais links do Youtube");
   ctx.session.command = "baixar";
 }
 
-async function handleMessages(ctx: MyContext) {
+// MIDDLEWARE FOR HANDLING MESSAGES
+// MIDDLEWARE FOR HANDLING MESSAGES
+bot.on("message", async (ctx) => {
   console.log("Received a message");
   console.log(ctx.session);
 
-  if (ctx.session.command === "baixar") {
-    await baixarCommand(ctx);
-  } else if (ctx.session.command !== "baixar") {
-    replyWithIntro(ctx);
-  }
-}
+  if (ctx.session.command === "baixar" && "text" in ctx.message) {
+    // Communicate with the API
+    const url =
+      "https://nominally-pleasing-herring.ngrok-free.app/api/download/";
+    ctx.reply("Iniciando o download das músicas...");
 
-function replyWithIntro(ctx: MyContext) {
-  ctx.reply(introductionMessage);
-}
+    let isProcessing = true;
+
+    // Set a timeout to send a message if processing takes too long
+    const timeoutId = setTimeout(() => {
+      if (isProcessing) {
+        ctx.reply("Processando Músicas. Por favor, aguarde...");
+      }
+    }, 5000); // Timeout after 30 seconds. Adjust this value as needed.
+
+    try {
+      const response = await axios.post(url, ctx.message.text, {
+        headers: { "Content-Type": "text/plain" },
+      });
+
+      // Cancel the timeout
+      clearTimeout(timeoutId);
+      isProcessing = false;
+
+      // Send response back to the user
+      // console.log(JSON.stringify(response));
+      ctx.reply("Músicas prontas para baixar");
+      ctx.reply("👍");
+    } catch (error) {
+      console.error(`Erro ao baixar músicas: ${error}`);
+      ctx.reply(
+        "Ocorreu um erro ao baixar as músicas. Por favor, tente novamente mais tarde."
+      );
+    }
+  } else {
+    ctx.reply(introductionMessage);
+  }
+});
 
 // BOT STARTUP
 if (process.env.NODE_ENV === "production") {
@@ -77,5 +105,5 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // MESSAGE DEFINITIONS
-const introductionMessage = `Aqui estão todos os comandos disponíveis:
-- /baixar: Baixar músicas a partir de URLs do YouTube`;
+const introductionMessage = `Here are all available commands:
+- /baixar: Baixar musicas`;
